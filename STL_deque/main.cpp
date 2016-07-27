91a790650054a3e4bed76161e7f112a61ae48a8f
Chaturvedi, Akash <Akash.Chaturvedi@sabre.com>
2016-07-27 13:37:17 -0500

    simple common operations for sequence containers (iterator traverasal and push_back operations)

--- STL_deque/main.cpp
+++ STL_deque/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
+#include <deque>
+//deque is basically short form of Double Ended Queue
+//SEQUENCE CONTAINER
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    deque<int> my_deque;
+    cout<<"deque initial size is : "<<my_deque.size()<<endl;
+    my_deque.push_back(420);
+    my_deque.push_back(9);
+    my_deque.push_back(2);
+    my_deque.push_back(11);
+    cout <<"updated size of deque is : "<<my_deque.size()<< endl;
+
+    deque<int>::iterator itr_begin = my_deque.begin();
+    deque<int>::iterator itr_end = my_deque.end();
+    deque<int>::iterator itr_temp = itr_begin;
+    while(itr_temp != itr_end){
+        cout<<*itr_temp<<" ";
+        itr_temp++;
+    }
+    cout<<endl;
     return 0;
 }
--- STL_vectors/main.cpp
+++ STL_vectors/main.cpp
@@ -1,9 +1,71 @@
-#include <iostream>
+/*
+ *This program demonstrate the use of a vector
+ *Vector is a dynamic array that can grow dynamically
+ *Obviously the memory is allocated from heap
+	|1|2|5|12|3|
+	 ^v.begin() ^v.end()
 
+	Hence,
+		cout<<*v.end();		//dangerous situation
+
+ *Properties of vector:
+	->fast insert/remove at the end:		O(1)
+	->slow insert/remove at the middle/beginning:	O(n)
+	->slow search:					O(n)
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	vector<int>v;			//v.size() is zero initially
+	//Adding elements to the vector v using pushback function
+	v.push_back(4);
+	v.push_back(1);
+	v.push_back(8);			//v.size is 3 now;
+
+					//v[2] is 8 (no range check in this one)
+					//v.at(2); is also 8 (throws range_error in case of exception)
+	vector<int>::iterator itr1 = v.begin();	//half open [begin, end)
+	vector<int>::iterator itr2 = v.end();
+
+	vector<int>::iterator itr = itr1;
+
+//Approach 1 ; recommended traversal 				[UNIVERSAL WAY FOR TRAVERSING A CONTAINER]
+    cout<<"First approach :"<<endl;
+	while(itr != itr2){
+		cout<<*itr<<endl;
+		itr++;
+	}
+//Approach 2 : however, can traverse a vector this way as well
+    cout<<"2nd approach :"<<endl;
+	int i;
+	for(i = 0; i<v.size(); i++)
+		cout<<v[i]<<endl;
+//Approach 3 : OR this way
+    cout<<"3rd approach :"<<endl;
+	for(i = 0; i<v.size(); i++)
+		cout<<v.at(i)<<endl;
+
+//Approach 4 : C++ 11
+    cout<<"4th approach :"<<endl;
+  //  vector<int>::iterator it = v.begin();
+	for(int it: v)
+		cout<<it<<endl;
+
+
+/* Vector is dynamically allocated contiguous array in memory
+ * None of other containers provide contiguous data in memory */
+
+	sort(itr1, itr2);	//Remember: Algorithms directly work on iterator, not on elements
+
+	cout<<"After sorting.."<<endl;
+	for(int it: v)
+		cout<<it<<endl;
+
+	return 0;
 }
+
+
